9111d1c42ca0cdd421babfc710e86fb235e19e0d
Victor Almeida França <fr4ncacc@gmail.com>
2019-02-18 08:36:41 -0300

    dashboard ranking and chart

--- js/main.js
+++ js/main.js
@@ -11,3 +11,49 @@ function toggleNav() {
   });
   main.classList.toggle("active");
 }
+
+const ctx = document.querySelector("#myChart").getContext("2d");
+const myChart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: [
+      "Thaymãe",
+      "Ronan",
+      "Victor França",
+      "João Paulo",
+      "Lucas Freitas"
+    ],
+    datasets: [
+      {
+        label: "# de pontos",
+        data: [1460, 1340, 1245, 1210, 1186],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)"
+        ],
+        borderColor: [
+          "rgba(255,99,132,1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)"
+        ],
+        borderWidth: 1
+      }
+    ]
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true
+          }
+        }
+      ]
+    }
+  }
+});
